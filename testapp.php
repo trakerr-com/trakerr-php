dc54c4762ad91488e2369bea030ee1facc9fbf5a
RMSD <rangers1500@yahoo.com>
2017-03-30 11:36:32 -0700

    Updated testapp.php. Added more examples, unified examples to fit one naming convention. Fixed a few examples here and there.

--- testapp.php
+++ testapp.php
@@ -9,5 +9,35 @@ $trakerrClient = new \trakerr\TrakerrClient("db91c9dd3cf14c8ce437be1198665d29197
 try {
     throw new Exception("test exception");
 } catch (Exception $e) {
-    $trakerr_client->sendError("Error", $e);
+    $trakerrClient->sendError($e, "fatal");
 }
+
+ // Option-4: send any event programmatically
+$appEvent = $trakerrClient->createAppEvent("warning", "type warn", "TestType", "Test message from php");
+
+$trakerrClient->sendEvent($appEvent);
+
+// Option-3: catch and send error to Trakerr with some custom data programmatically
+use trakerr\client\model\CustomData;
+use trakerr\client\model\CustomStringData;
+
+try {
+    throw new Exception("Too much math");
+} catch (Exception $e) {
+    $appEvent2 = $trakerrClient->createAppEventFromException($e, "Error");
+
+    // set some custom data
+    $customProperties = new CustomData();
+    $customStringData = new CustomStringData();
+    $customStringData->setCustomData1("Some custom data");
+    $customProperties->setStringData($customStringData);
+    $appEvent2->setCustomProperties($customProperties);
+
+    $trakerrClient->sendEvent($appEvent2);
+}
+
+// Option-1: register global exception handlers (optional)
+$trakerrClient->registerErrorHandlers();
+
+throw new Exception("Not enough math");
+
